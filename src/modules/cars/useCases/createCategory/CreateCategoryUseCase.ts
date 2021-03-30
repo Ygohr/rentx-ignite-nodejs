8cff3bc03f3bad2ed98204a414089e9abac6e984
Ygohr <medeirosygohr@gmail.com>
2021-03-30 20:59:43 -0300

    first commit

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -0,0 +1,20 @@
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+class CreateCategoryUseCase {
+  constructor(private categoriesRepository: ICategoriesRepository) {}
+  execute({ name, description }: IRequest): void {
+    const categoryExists = this.categoriesRepository.findByName(name);
+
+    if (categoryExists) {
+      throw new Error("Category already exists");
+    }
+    this.categoriesRepository.create({ name, description });
+  }
+}
+
+export { CreateCategoryUseCase };
