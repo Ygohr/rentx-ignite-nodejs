0a8653fd69e704c5b4703aaac6cdba765e7a46ef
Ygohr <medeirosygohr@gmail.com>
2021-04-07 21:16:06 -0300

    initiate users items

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,4 +1,6 @@
 import { container } from "tsyringe";
+import { UsersRepository } from "../../modules/accounts/implementations/UsersRepository";
+import { IUsersRepository } from "../../modules/accounts/repositories/IUsersRepository";
 import { ICategoriesRepository } from "../../modules/cars/repositories/ICategoriesRepository";
 import { CategoriesRepository } from "../../modules/cars/repositories/implementations/CategoriesRepository";
 import { SpecificationsRepository } from "../../modules/cars/repositories/implementations/SpecificationsRepository";
@@ -6,3 +8,4 @@ import { ISpecificationsRepository } from "../../modules/cars/repositories/ISpec
 
 container.registerSingleton<ICategoriesRepository>("CategoriesRepository", CategoriesRepository);
 container.registerSingleton<ISpecificationsRepository>("SpecificationsRepository", SpecificationsRepository);
+container.registerSingleton<IUsersRepository>("UsersRepository", UsersRepository);
