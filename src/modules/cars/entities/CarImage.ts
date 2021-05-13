ea9a4e29de4d68a9df81af1f254d04458cdf4a5c
Ygohr <medeirosygohr@gmail.com>
2021-05-13 20:30:29 -0300

    initiate rentals

--- src/modules/cars/entities/CarImage.ts
+++ src/modules/cars/entities/CarImage.ts
@@ -1,4 +1,4 @@
-import { Column, CreateDateColumn, Entity, ManyToOne, OneToMany, PrimaryColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
 import { v4 as uuidV4 } from "uuid";
 
 @Entity("cars_image")
