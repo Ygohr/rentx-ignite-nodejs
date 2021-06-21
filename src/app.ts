a35684c0f142d0352c4805e86a99037d740cf948
Ygohr <medeirosygohr@gmail.com>
2021-06-21 19:54:33 -0300

    add cors

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import express from "express";
 import swaggerUi from "swagger-ui-express";
 import errorMiddleware from "@shared/middlewares/errorMiddleware";
 import upload from "@config/upload";
+import cors from "cors";
 import { router } from "./routes";
 import swaggerFile from "./swagger.json";
 import createConnection from "./database";
@@ -20,6 +21,7 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
+app.use(cors());
 app.use(router);
 
 app.use(errorMiddleware);
