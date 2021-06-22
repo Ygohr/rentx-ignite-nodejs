63f243c15d426b648e35384ef276468bea89fea1
Ygohr <medeirosygohr@gmail.com>
2021-06-21 23:09:26 -0300

    add sentry

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,8 @@ import errorMiddleware from "@shared/middlewares/errorMiddleware";
 import rateLimiter from "@shared/middlewares/rateLimiter";
 import upload from "@config/upload";
 import cors from "cors";
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 import { router } from "./routes";
 import swaggerFile from "./swagger.json";
 import createConnection from "./database";
@@ -17,6 +19,15 @@ const app = express();
 
 app.use(rateLimiter);
 
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [new Sentry.Integrations.Http({ tracing: true }), new Tracing.Integrations.Express({ app })],
+  tracesSampleRate: 1.0
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+
 app.use(express.json());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
@@ -26,6 +37,8 @@ app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
 app.use(cors());
 app.use(router);
+
+app.use(Sentry.Handlers.errorHandler());
 
 app.use(errorMiddleware);
 
